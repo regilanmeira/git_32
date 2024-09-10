09717cfefbab159ea6968f0e27cd54e2ff05597b
regilanmeira <regilan@hotmail.com>
2024-09-10 11:52:26 -0300

    Adicionado link para página 1

--- index.php
+++ index.php
@@ -10,5 +10,6 @@
     <h1>Versionando código com GIT</h1>
     <h2>Aula 10/09</h2>
     <p>Nesta aula aprendemos a versionar de forma local</p>
+    <a href="pagina1.php">Ir para página 1</a>
 </body>
 </html>
